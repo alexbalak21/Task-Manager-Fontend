c15912bc3b30e851d30dfe50f47c92324f993787
alexbalak21 <alex.balak@outlook.com>
2026-04-19 23:37:46 +0200

    Creating todo implementation

--- src/modules/Tasks/components/CreateTask/components/TaskTodoList.tsx
+++ src/modules/Tasks/components/CreateTask/components/TaskTodoList.tsx
@@ -1,5 +1,5 @@
-import { Plus } from "lucide-react";
-import type { KeyboardEvent } from "react";
+import { Plus, Trash2 } from "lucide-react";
+import { useState, type KeyboardEvent } from "react";
 import type { TodoItem } from "../hooks/useTodoList";
 
 interface TaskTodoListProps {
@@ -19,11 +19,22 @@ export default function TaskTodoList({
   onRemove,
   onKeyDown,
 }: TaskTodoListProps) {
+  const [todos, setTodos] = useState<string[]>([]);
+  const addTodo = () => {    
+    if (input.trim() === "") return;
+    setTodos([...todos, input.trim()]);
+    onInput("");
+  }
   return (
     <div>
       <label className="mb-2 block text-sm font-semibold text-gray-900 dark:text-gray-100">
         TODO Checklist
       </label>
+
+      {/* LOCKED - TODO LIST COMPONENT */}
+      <div className="mb-4 rounded-lg bg-gray-50 border border-gray-300 px-3 py-3 flex text-base">
+      <span className="text-gray-500 me-4">{"01"}</span><span className="">{"Do Something"}</span><button className="text-red-600 ms-auto"><Trash2/></button>
+      </div>
 
       <div className="flex gap-2">
         <input
@@ -36,10 +47,10 @@ export default function TaskTodoList({
         />
         <button
           type="button"
-          onClick={onAdd}
+          onClick={addTodo}
           className="rounded-lg bg-zinc-400 px-4 text-white"
         >
-          <Plus size={20} />
+         <Plus size={20} />
         </button>
       </div>
 
